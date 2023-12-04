556b5145bbed6e976880a616e99c03c210e4faab
Nick Sotula <so2niko@gmail.com>
2023-12-04 22:18:09 +0200

    Create one.js

---
+++ js-1-2-231204/one.js
@@ -0,0 +1,82 @@
+const numbers = 22;
+const numbers2 = 1.43434343;
+const numbers3 = -.413;
+const numbers4 = -13;
+const numbers5 = -13.2222; 
+console.log(numbers3);
+const string = 'Hello, Steven\'s';
+const string2 = "           H el l    o  Steven's";
+const string3 = `He
+d
+
+dsf  ${numbers}
+ds
+fds
+f ${ 'H' > 22 }
+ds
+llo`;
+console.log(string3);
+const boolean = true;
+const boolean2 = false;
+console.log(boolean);
+const un = undefined;
+let un2;
+console.log(un);
+console.log(un2);
+const n = null;
+console.log(n);
+
+let dob = 2000;
+let dob2 = dob + 3;
+console.log(dob);
+dob = dob2;
+console.log(dob);
+
+const o = {};
+o.ii3 = 'Mykola';
+o['---~3424234i i3'] = 'Mykola';
+console.log(o);
+const variables = 'a-zA-Z0-9$_';
+const _4235ddsfadsf = 3;
+const $3apples = '3 apples';
+//TODO: variable naming - Правила іменування змінних(стилі іменування - pascal case, snake case...)
+//TODO: Системи числення
+//TODO: Big Int vs Symbol
+
+const user1 = {
+	name : 'Mykola',
+  isOnTable : true,
+};
+
+let user2 = user1;
+
+user2.name = 'Ludmyla';
+user1.name = 'Andrii';
+
+const user3 = user2;
+/* user3 = 2222222222; */
+console.log('-------------------');
+console.log('user1', user1);
+console.log('user2', user2);
+console.log('user3', user3);
+
+//-----------------------------------
+
+
+//змінна
+let a = 33;
+let b;
+//константи
+const c = 22;
+/* const d; */
+
+//застарілий
+var e = {};
+
+//не рекомендований
+f = 342;
+
+/* let a = 1; */
+//||
+
+f = 'fdsfds'
