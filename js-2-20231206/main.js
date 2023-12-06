0c7690f3db71ab7ea61b5fe6471296b7d1073e4f
Nick Sotula <so2niko@gmail.com>
2023-12-06 20:14:22 +0000

    feat: operators

---
+++ js-2-20231206/main.js
@@ -0,0 +1,28 @@
+console.log('Hello');
+
+//Компілятор
+//перетворює програмний код в код, що розуміє ЕОМ
+
+//Інтепретатор
+//на льоту перетворює і запускає програмний код
+
+let four = 4;
+let eleven = 11;
+
+//Операція, оператор, операнд
+console.log(3 + 4);
+// Операція - додавання
+// Оператор + знак плюс
+// Операнд - операнд 1 - 3, операнд 2 - 4
+console.log(1 - 2);
+console.log(3 * 4);
+console.log(10 / 3);
+console.log(2 ** 4);
+console.log(-3 * -5);
+console.log(--four);// four = four - 1; - дикремент
+console.log(++eleven); // eleven = eleven + 1
+console.log(eleven);
+console.log((3 + 3) * 3);
+console.log('New' + 'York');
+console.log('2' + 2);
+console.log('2' * 3);
